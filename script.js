7f1934795106e8fa8ec2ce8518d7a766aade9f7d
Jayram Dangi <jayd95495@gmail.com>
2025-01-26 20:32:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+// till 38:00 git 
+
+
+// now there is one problem that 
+// if we are working as a team in a project 
+
+//  then my code is present at my system 
+// how to share it with the member of my team 
+
+
+/// write the code push it to google drive
+// he will download it from here 
+
+// {code1}->{code2}->{code3}
+// now we have to assign two subtasks to members 
+// one login and one payment 
+// here branching comes and then we will merge that 
+// code
+
+
+// but how to share with other members
+
+// so there should be a central thing where they 
+// both have there code 
+// github provide that central thing 
+
+
+// example 
+
+// basic code of the website is in the github 
+// two subtasks are given login and payment 
+
+// now in my system i am doing work on payment 
+// now i will push the payment one on github
+
+// now my friend is working on the login one 
+// and then he pushes on the github 
+
+// company watches the commit for the 
+// promotion and layoffs 
+
+
+
+
+
+
